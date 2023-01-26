a6b5d0e70005dc3ce1314acd319b55e005dbae73
Adão Gaspar Bartolomeu <adaobartolomeua.b96@hotmail.com>
2023-01-26 08:23:39 +0100

    add move frog logic

--- app.js
+++ app.js
@@ -4,9 +4,28 @@ const startPauseButton = document.querySelector('#start-pause-button')
 const squares = document.querySelectorAll('.grid div')
 
 let currentIndex = 76
+const widthSquare = 9
 
-function moveFrog(){
+function moveFrog(e){
    
+    squares[currentIndex].classList.remove('frog')
+
+    switch(e.key){
+        case 'ArrowLeft':
+
+        if(currentIndex  % widthSquare !== 0) currentIndex -= 1
+            break;
+        case 'ArrowRight':
+            if(currentIndex  % widthSquare < widthSquare -1 ) currentIndex += 1
+            break;
+        case 'ArrowUp':
+            if(currentIndex - widthSquare >=0)  currentIndex -= widthSquare
+            break;
+        
+        case 'ArrowDown':
+            if(currentIndex + widthSquare < widthSquare * widthSquare) currentIndex += widthSquare
+            break;
+    }
     squares[currentIndex].classList.add('frog')
 }
 
